91791717752ced36ac041e85f2489454fe1c650c
Aluno <aluno@band.net>
2017-04-07 20:35:01 -0300

    commit 20:34 - podem começar a clonargit add .

---
+++ app/src/main/java/com/example/s2pet/s2pet/Cadastro_Pet.java
@@ -0,0 +1,13 @@
+package com.example.s2pet.s2pet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro_Pet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro__pet);
+    }
+}
---
+++ app/src/main/java/com/example/s2pet/s2pet/Home.java
@@ -0,0 +1,13 @@
+package com.example.s2pet.s2pet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
