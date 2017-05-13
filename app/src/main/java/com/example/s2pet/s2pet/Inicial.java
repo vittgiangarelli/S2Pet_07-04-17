d2f98fe8f0e37b0f1b90befec72d77ef7f2bf73c
Aluno <aluno@band.net>
2017-05-12 21:33:06 -0300

    será?

--- app/src/main/java/com/example/s2pet/s2pet/Inicial.java
+++ app/src/main/java/com/example/s2pet/s2pet/Inicial.java
@@ -8,6 +8,6 @@ public class Inicial extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicial);
+        setContentView(R.layout.splash);
     }
 }
---
+++ app/src/main/java/com/example/s2pet/s2pet/Splash.java
@@ -0,0 +1,45 @@
+package com.example.s2pet.s2pet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.RotateDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Space;
+import android.os.Handler;
+
+
+import static com.example.s2pet.s2pet.R.layout.splash;
+
+/**
+ * Created by aluno on 05/05/2017.
+ */
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent homeIntent = new Intent(Splash.this, Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
