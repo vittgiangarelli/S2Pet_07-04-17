03619c55aa88b879f5c1af5441a8878937b95a02
Aguinaldo Almeida <Aguinaldo Almeida>
2017-05-12 22:37:22 -0300

    conectando com o banco

--- app/src/main/java/com/example/s2pet/s2pet/Cadastro_Dono.java
+++ app/src/main/java/com/example/s2pet/s2pet/Cadastro_Dono.java
@@ -3,6 +3,10 @@ package com.example.s2pet.s2pet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Cadastro_Dono extends AppCompatActivity {
 
     @Override
@@ -10,4 +14,40 @@ public class Cadastro_Dono extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro__dono);
     }
+
+    public class Dono {
+        private String id;
+        private String nomeDono;
+        private List<String> listaPet;
+
+        public Dono(){}
+
+        public Dono(String id, String nomeDono, List<String> listaPet ) {
+            this.nomeDono = nomeDono;
+            this.listaPet = listaPet;
+            this.id = id;
+        }
+
+        // TODO 10 - criar um mapa de valores
+        public Map<String, Object> mapaDeValores(){
+            HashMap<String,Object> resutado = new HashMap<>();
+            resutado.put("id", id);
+            resutado.put("nomeDono", nomeDono);
+            resutado.put("listaPet", listaPet);
+            return resutado;
+        }
+
+        public List<String> getListaPet() {
+            return listaPet;
+        }
+
+        public String getNomeDono() {
+            return nomeDono;
+        }
+
+        public String getid() {
+            return id;
+        }
+    }
+
 }
--- app/src/main/java/com/example/s2pet/s2pet/Home.java
+++ app/src/main/java/com/example/s2pet/s2pet/Home.java
@@ -3,11 +3,21 @@ package com.example.s2pet.s2pet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Home extends AppCompatActivity {
+
+
+    private FirebaseDatabase database; //TODO 6 - linha que cria uma instâcia de um obejto do tipo FirebaseDatabase
+    private DatabaseReference reference; //TODO 7 - linha que cria uma instâcia de um obejto do tipo DatabaseReference
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        database =  FirebaseDatabase.getInstance();  //TODO 8 - esta linha atribui a variável a instâcia da base de dados do Firebase
+        reference = database.getReference("Donos");  //TODO 9 - esta linha atribui a variável uma referência("tabela") da base de dados
     }
 }
