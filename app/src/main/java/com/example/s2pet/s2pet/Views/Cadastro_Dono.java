ec25b2f0ef9e4034441cb71baa920a1b049aedbc
Marcos Paulo <ooisoupaulo@gmail.com>
2017-06-12 22:37:52 -0300

    adicionando novamente

--- app/src/main/java/com/example/s2pet/s2pet/Views/Cadastro_Dono.java
+++ app/src/main/java/com/example/s2pet/s2pet/Views/Cadastro_Dono.java
@@ -8,12 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.s2pet.s2pet.Models.Dono;
 import com.example.s2pet.s2pet.R;
 import com.example.s2pet.s2pet.Utils.Referencias;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Cadastro_Dono extends AppCompatActivity {
@@ -24,7 +27,7 @@ public class Cadastro_Dono extends AppCompatActivity {
     private EditText nomePet;
     private EditText vacina;
     private TextView lista;
-    // private List<String> listaVacinas;
+    private List<String> listaVacinas;
     private FirebaseDatabase database; //TODO 6 - linha que cria uma instâcia de um obejto do tipo FirebaseDatabase
     private DatabaseReference reference; //TODO 7 - linha que cria uma instâcia de um obejto do tipo DatabaseReference
     Button btn;
@@ -39,9 +42,9 @@ public class Cadastro_Dono extends AppCompatActivity {
         email = (EditText) findViewById(R.id.email);
         nomePet = (EditText) findViewById(R.id.NomePet);
         vacina = (EditText) findViewById(R.id.vacina);
-        //lista = (TextView) findViewById(R.id.listaVacinas);
+        lista = (TextView) findViewById(R.id.listaVac);
         btn = (Button) findViewById(R.id.button3);
-        // listaVacinas = new ArrayList<>();
+        listaVacinas = new ArrayList<>();
         database = FirebaseDatabase.getInstance();  //TODO 8 - esta linha atribui a variável a instâcia da base de dados do Firebase
         reference = database.getReference(Referencias.REFERENCIADONO);  //TODO 9 - esta linha atribui a variável uma referência("tabela") da base de dados
 
@@ -49,23 +52,23 @@ public class Cadastro_Dono extends AppCompatActivity {
 
     public void salvarDados(View view) {
 
-        // Dono dono = new Dono(cpf.getText().toString(), nomeDono.getText().toString(),email.getText().toString(), nomePet.getText().toString(), listaVacinas); // TODO 12 - instânciar um objeto Dono e passar os valores que serão salvos pelo construtor
+        Dono dono = new Dono(cpf.getText().toString(), nomeDono.getText().toString(),email.getText().toString(), nomePet.getText().toString(), listaVacinas); // TODO 12 - instânciar um objeto Dono e passar os valores que serão salvos pelo construtor
 
-        //  Map<String, Object> valoresDono = dono.mapaDeValores(); //TODO 13 - atribuir o mapa de valores da classe Dono a variavel
+        Map<String, Object> valoresDono = dono.mapaDeValores(); //TODO 13 - atribuir o mapa de valores da classe Dono a variavel
         Map<String, Object> enviarValores = new HashMap<>(); //TODO 14 - criar um hashMap que é quem vai ser reponsável por "cruzar os dados"
-        // enviarValores.put(cpf.getText().toString(), valoresDono); // TODO 15 - aqui será passado para o hash chave e valor que serão enviados para o Firebase
+        enviarValores.put(cpf.getText().toString(), valoresDono); // TODO 15 - aqui será passado para o hash chave e valor que serão enviados para o Firebase
 
         reference.updateChildren(enviarValores); // TODO 16 - esta linha é responsável por enviar os dados para o Firebase
 
         nomeDono.setText("");
-        //  listaVacinas.clear();
+            listaVacinas.clear();
         lista.setText("");
     }
 
     public void adicionarVacina(View v) {
         String nome = vacina.getText().toString();
 
-     /*   listaVacinas.add(nome);
+     listaVacinas.add(nome);
         String listaNomes = "";
 
         if(!listaVacinas.isEmpty()){
@@ -76,7 +79,7 @@ public class Cadastro_Dono extends AppCompatActivity {
             lista.setText(listaNomes);
             vacina.setText("");
         }
-        */
+
     }
 
     public void irPerfil(View v) {
